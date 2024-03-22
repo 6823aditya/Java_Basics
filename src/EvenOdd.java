b1044fecc26e584b6d3287aa87b6b21043765020
6823aditya <6823.aditya@gmail.com>
2024-03-22 11:21:06 +0530

    Initial commit

---
+++ src/EvenOdd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Basics {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number:");
+        int number = input.nextInt();
+        if(number%2==0){
+            System.out.println(number +" is an even number");
+        }
+        else{
+            System.out.println(number +" is an odd number");
+        }
+        input.close();
+
+
+
+
+
+    }
+}
