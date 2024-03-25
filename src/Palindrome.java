7927370f5d4cd75dbeb2ba7f7fd81fe157f38c58
6823aditya <6823.aditya@gmail.com>
2024-03-25 21:36:53 +0530

    Palindrome.java

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -6,6 +6,29 @@ public class Palindrome {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter string: ");
         String a = input.next();
+        String[] arr = a.split("");
+        String Str_rev = "";
+        int i;
+        i=a.length();
+        while(i!=0){
+            System.out.println(" "+arr[i-1]);
+            Str_rev+=arr[i-1];
+            i--;
+        }
+        if(Str_rev.equals(a) ){
+            System.out.print(a+" is a palindrome string");
+
+        }else{
+            System.out.println(a+" is not a palindrome string");
+        }
+        System.out.println(Str_rev);
+        System.out.println(a);
+
+
+
+
+
+
 
 
 
